27298a435b96cc3a32adc42219d0e46c6e01e9d5
Simon Relet <simon.relet@blablacar.com>
2021-01-15 22:26:42 +0100

    feat(api): Add check for env variables

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,6 +1,26 @@
 import "./env";
 import { Server } from "./server";
 
+const missingEnvVariables = [
+  "NODE_ENV",
+  "PORT",
+  "FIREBASE_PROJECT_ID",
+  "FIREBASE_CLIENT_EMAIL",
+  "FIREBASE_DATABASE_URL",
+  "FIREBASE_PRIVATE_KEY",
+  "ALGOLIA_ID",
+  "ALGOLIA_ADMIN_KEY",
+].filter((value) => process.env[value] == null);
+
+if (missingEnvVariables.length > 0) {
+  throw new Error(
+    [
+      `Missing environment variables: ${missingEnvVariables.join(", ")}.`,
+      "Please check they are correctly defined in the .env* files.",
+    ].join("\n")
+  );
+}
+
 async function main() {
   Server.start();
 }
