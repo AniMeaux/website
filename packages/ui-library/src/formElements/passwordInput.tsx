07e514b285a0085c99722970d746d0f6407a702f
Simon Relet <simon.relet@blablacar.com>
2021-01-19 10:57:54 +0100

    feat(ui-library): Focus back the input after revealing the password

--- packages/ui-library/src/formElements/passwordInput.tsx
+++ packages/ui-library/src/formElements/passwordInput.tsx
@@ -5,16 +5,26 @@ import { Input, InputProps } from "./input";
 
 export function PasswordInput(props: InputProps) {
   const [showPassword, setShowPassword] = React.useState(false);
+  const inputElement = React.useRef<HTMLInputElement>(null!);
 
   return (
     <Input
       {...props}
       type={showPassword ? "text" : "password"}
       rightAdornment={
-        <ActionAdornment onClick={() => setShowPassword((s) => !s)}>
+        <ActionAdornment
+          onClick={() => {
+            setShowPassword((s) => !s);
+
+            // Give the focus {back} to the input to avoid loosing the keyboard
+            // on mobile apps
+            inputElement.current.focus();
+          }}
+        >
           {showPassword ? <FaEyeSlash /> : <FaEye />}
         </ActionAdornment>
       }
+      refProp={inputElement}
     />
   );
 }
