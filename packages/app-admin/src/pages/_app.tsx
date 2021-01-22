c7dd6f330ec4398dd44232bfcae3435d9e9efe5c
Simon Relet <simon.relet@blablacar.com>
2021-01-22 11:40:53 +0100

    refactor(app-admin): Use env variables for app name

--- packages/app-admin/src/pages/_app.tsx
+++ packages/app-admin/src/pages/_app.tsx
@@ -24,9 +24,18 @@ export function PageHead() {
   return (
     <Head>
       <meta charSet="utf-8" />
-      <meta name="application-name" content="Admin Ani'Meaux" />
-      <meta name="apple-mobile-web-app-title" content="Admin Ani'Meaux" />
-      <meta name="description" content="Admin app for Ani'Meaux" />
+      <meta
+        name="application-name"
+        content={process.env.NEXT_PUBLIC_APP_NAME}
+      />
+      <meta
+        name="apple-mobile-web-app-title"
+        content={process.env.NEXT_PUBLIC_APP_NAME}
+      />
+      <meta
+        name="description"
+        content={process.env.NEXT_PUBLIC_APP_DESCRIPTION}
+      />
       <link rel="manifest" href="/manifest.json" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
