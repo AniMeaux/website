4cfdd2862672abb02978e30456c39c45a977c6f0
Simon Relet <simon.relet@blablacar.com>
2021-02-12 23:14:48 +0100

    feat(ui-library): Add FieldMessage placement

--- libraries/ui-library/src/formElements/fieldMessage.tsx
+++ libraries/ui-library/src/formElements/fieldMessage.tsx
@@ -1,25 +1,40 @@
 import cn from "classnames";
 import * as React from "react";
+import { StyleProps } from "../core";
 
-type FieldMessageProps = Omit<
-  React.HTMLAttributes<HTMLParagraphElement>,
-  "children"
-> & {
+type FieldMessagePlacement = "top" | "bottom";
+
+const FieldMessagePlacementClassName: {
+  [key in FieldMessagePlacement]: string;
+} = {
+  bottom: "mt-1",
+  top: "mb-1",
+};
+
+type FieldMessageProps = StyleProps & {
+  placement?: FieldMessagePlacement;
   infoMessage?: React.ReactNode;
   errorMessage?: React.ReactNode;
 };
 
-const COMMON_CLASS_NAME = "mt-1 px-4 text-xs";
+const COMMON_CLASS_NAME = "px-4 text-xs";
 
 export function FieldMessage({
+  placement = "bottom",
   infoMessage,
   errorMessage,
   className,
-  ...rest
 }: FieldMessageProps) {
   if (errorMessage != null) {
     return (
-      <p {...rest} className={cn(COMMON_CLASS_NAME, "text-red-500", className)}>
+      <p
+        className={cn(
+          COMMON_CLASS_NAME,
+          FieldMessagePlacementClassName[placement],
+          "text-red-500",
+          className
+        )}
+      >
         {errorMessage}
       </p>
     );
@@ -28,9 +43,9 @@ export function FieldMessage({
   if (infoMessage != null) {
     return (
       <p
-        {...rest}
         className={cn(
           COMMON_CLASS_NAME,
+          FieldMessagePlacementClassName[placement],
           "text-black text-opacity-60",
           className
         )}
