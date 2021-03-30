ac1e5dd891c8d64df10608d3c11639d106b87947
Simon Relet <simon.relet@blablacar.com>
2021-03-30 22:29:21 +0200

    feat(website): Update HP styles

--- apps/website/src/core/header/header.tsx
+++ apps/website/src/core/header/header.tsx
@@ -1,13 +1,15 @@
 import { Link } from "@animeaux/ui-library/build/core/link";
 import * as React from "react";
+import Logo from "../../ui/logo.svg";
 import NameAndLogo from "../../ui/nameAndLogo.svg";
 import { Navigation } from "../navigation";
 
 export function Header() {
   return (
     <header className="Header">
-      <Link href="/" className="HeaderLogo">
-        <NameAndLogo />
+      <Link href="/" className="HeaderLogoLink">
+        <NameAndLogo className="HeaderNameAndLogo" />
+        <Logo className="HeaderLogo" />
       </Link>
 
       <Navigation />
