b7ab8ab3254743d1615732527a21b6be9d975c66
Simon Relet <simon.relet@blablacar.com>
2021-03-30 12:24:07 +0200

    feat(website): Add adoptable animals section

--- apps/website/src/pages/index.tsx
+++ apps/website/src/pages/index.tsx
@@ -1,12 +1,3 @@
-import { publicFetchGraphQL } from "@animeaux/app-core/build/request/publicFetchGraphQL";
-import { PublicAnimal } from "@animeaux/shared-entities/build/animal";
-import {
-  PaginatedRequestParameters,
-  PaginatedResponse,
-} from "@animeaux/shared-entities/build/pagination";
-import * as Sentry from "@sentry/react";
-import { gql } from "graphql-request";
-import { GetServerSideProps } from "next";
 import * as React from "react";
 import { Footer } from "../core/footer";
 import { Header } from "../core/header";
@@ -19,73 +10,7 @@ import {
   StatisticsSection,
 } from "../ui/statisticsSection";
 
-const PublicAnimalFragment = gql`
-  fragment PublicAnimalFragment on PublicAnimal {
-    id
-    officialName
-    birthdate
-    gender
-    species
-    breed {
-      id
-      name
-      species
-    }
-    color {
-      id
-      name
-    }
-    avatarId
-    isOkChildren
-    isOkDogs
-    isOkCats
-    isSterilized
-  }
-`;
-
-const GetAllAdoptableAnimalsQuery = gql`
-  query GetAllAdoptableAnimals($page: Int) {
-    response: getAllAdoptableAnimals(page: $page) {
-      hits {
-        ...PublicAnimalFragment
-      }
-      hitsTotalCount
-      page
-      pageCount
-    }
-  }
-
-  ${PublicAnimalFragment}
-`;
-
-export type HomePageProps = {
-  animals: PublicAnimal[];
-};
-
-export const getServerSideProps: GetServerSideProps<HomePageProps> = async () => {
-  let animals: PublicAnimal[] = [];
-
-  try {
-    const { response } = await publicFetchGraphQL<
-      { response: PaginatedResponse<PublicAnimal> },
-      PaginatedRequestParameters
-    >(GetAllAdoptableAnimalsQuery, {
-      variables: { page: 0 },
-    });
-
-    animals = response.hits;
-  } catch (error) {
-    Sentry.captureException(error, {
-      extra: { query: "getAllAdoptableAnimals" },
-    });
-  }
-
-  return { props: { animals } };
-};
-
-export default function HomePage({ animals }: HomePageProps) {
-  console.log("animals:", animals);
-
+export default function HomePage() {
   return (
     <>
       <PageTitle />
