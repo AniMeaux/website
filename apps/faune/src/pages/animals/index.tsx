30c36670ae202c4f9ff9441ccce7e125fe6c5174
Simon Relet <simon.relet@blablacar.com>
2021-04-14 20:11:44 +0200

    feat(faune): Display more info for active animals

--- apps/faune/src/pages/animals/index.tsx
+++ apps/faune/src/pages/animals/index.tsx
@@ -2,22 +2,93 @@ import {
   Header,
   PageComponent,
   renderInfiniteItemList,
-  SearchableAnimalItemPlaceholder,
-  SearchableAnimalLinkItem,
   useAllActiveAnimals,
   useCurrentUser,
 } from "@animeaux/app-core";
-import { doesGroupsIntersect, UserGroup } from "@animeaux/shared-entities";
 import {
+  AnimalStatus,
+  doesGroupsIntersect,
+  getAnimalDisplayName,
+  SearchableAnimal,
+  UserGroup,
+} from "@animeaux/shared-entities";
+import {
+  Avatar,
+  Image,
+  Item,
+  ItemContent,
+  ItemIcon,
+  ItemMainText,
+  ItemSecondaryText,
+  LinkItem,
   Main,
+  Placeholder,
   QuickLinkAction,
   Section,
   usePageScrollRestoration,
 } from "@animeaux/ui-library";
+import cn from "classnames";
 import * as React from "react";
 import { FaPlus, FaSearch } from "react-icons/fa";
 import { Navigation } from "../../core/navigation";
 import { PageTitle } from "../../core/pageTitle";
+
+const searchableAnimalItemPlaceholder = (
+  <Item>
+    <ItemIcon>
+      <Placeholder preset="avatar-large" />
+    </ItemIcon>
+
+    <ItemContent>
+      <ItemMainText>
+        <Placeholder preset="label" />
+      </ItemMainText>
+
+      <ItemSecondaryText>
+        <Placeholder preset="text" />
+      </ItemSecondaryText>
+    </ItemContent>
+  </Item>
+);
+
+const StatusBadgeColors: { [key in AnimalStatus]: string } = {
+  [AnimalStatus.ADOPTED]: "bg-green-500",
+  [AnimalStatus.DECEASED]: "bg-gray-800",
+  [AnimalStatus.FREE]: "bg-gray-800",
+  [AnimalStatus.OPEN_TO_ADOPTION]: "bg-blue-500",
+  [AnimalStatus.OPEN_TO_RESERVATION]: "bg-blue-500",
+  [AnimalStatus.RESERVED]: "bg-yellow-500",
+  [AnimalStatus.UNAVAILABLE]: "bg-gray-800",
+};
+
+function AnimalLinkItem({ animal }: { animal: SearchableAnimal }) {
+  const displayName = getAnimalDisplayName(animal);
+
+  return (
+    <LinkItem href={`./${animal.id}`}>
+      <ItemIcon className="relative">
+        <Avatar size="large">
+          <Image image={animal.avatarId} preset="avatar" alt={displayName} />
+        </Avatar>
+
+        <span
+          className={cn(
+            "absolute bottom-0 right-0 inline-flex h-4 w-4 rounded-full border-2 border-white",
+            StatusBadgeColors[animal.status]
+          )}
+        />
+      </ItemIcon>
+
+      <ItemContent>
+        <ItemMainText>{displayName}</ItemMainText>
+
+        {animal.hostFamily != null && (
+          <ItemSecondaryText>{animal.hostFamily.name}</ItemSecondaryText>
+        )}
+      </ItemContent>
+    </LinkItem>
+  );
+}
 
 const TITLE = "Animaux en charge";
 
@@ -34,11 +105,9 @@ const AnimalListPage: PageComponent = () => {
   const { content, title } = renderInfiniteItemList(query, {
     title: TITLE,
     getItemKey: (animal) => animal.id,
-    renderPlaceholderItem: () => <SearchableAnimalItemPlaceholder />,
+    renderPlaceholderItem: () => searchableAnimalItemPlaceholder,
     emptyMessage: "Il n'y a pas encore d'animaux",
-    renderItem: (animal) => (
-      <SearchableAnimalLinkItem animal={animal} href={`./${animal.id}`} />
-    ),
+    renderItem: (animal) => <AnimalLinkItem animal={animal} />,
   });
 
   return (
