c317fdf0b9c481fd0649a3da489f164a5d4575cc
Simon Relet <simon.relet@gmail.com>
2022-08-12 10:40:23 +0200

    💿✨ Add NumbersSection

--- apps/website-remix/src/routes/index.tsx
+++ apps/website-remix/src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { SearchForm } from "~/controllers/searchForm";
 import { cn } from "~/core/classNames";
 import { StaticImage, StaticImageProps } from "~/dataDisplay/image";
+import { Icon, IconProps } from "~/generated/icon";
 import { adoptionImages } from "~/images/adoption";
 import { fosterFamilySmallImages } from "~/images/fosterFamilySmall";
 import { heroImages } from "~/images/hero";
 import { pickUpImages } from "~/images/pickUp";
+import { BubbleShape } from "~/layout/bubbleShape";
 import { HeroSection } from "~/layout/heroSection";
 
 export default function HomePage() {
@@ -20,6 +22,7 @@ export default function HomePage() {
       />
 
       <WhoWeAreSection />
+      <NumbersSection />
     </main>
   );
 }
@@ -82,6 +85,82 @@ function WhoWeAreItem({
       />
 
       <p>{text}</p>
+    </li>
+  );
+}
+
+function NumbersSection() {
+  return (
+    <section className="relative flex">
+      {/* Wrap the shape because it looks like SVG can only be sized with width
+      and height. But we don't want the width class to be a complexe arbitrary
+      value with hard coded size in px: `w-[calc(100%_-_16px)]` */}
+      <span
+        className={cn(
+          "absolute -z-10 top-0 left-2 bottom-0 right-2",
+          "md:left-4 md:right-4"
+        )}
+      >
+        <BubbleShape className="w-full h-full" />
+      </span>
+
+      <ul
+        className={cn(
+          "w-full px-10 py-12 flex items-start flex-wrap justify-evenly gap-12",
+          "md:px-24 md:py-10"
+        )}
+      >
+        <NumberItem
+          icon="cakeCandles"
+          value="4 ans"
+          label="D'existence"
+          color="green"
+        />
+        <NumberItem
+          icon="handHoldingHeart"
+          value="500"
+          label="Prises en charge"
+          color="yellow"
+        />
+        <NumberItem
+          icon="peopleGroup"
+          value="50"
+          label="Bénévoles"
+          color="red"
+        />
+      </ul>
+    </section>
+  );
+}
+
+function NumberItem({
+  icon,
+  value,
+  label,
+  color,
+}: {
+  icon: IconProps["id"];
+  value: React.ReactNode;
+  label: React.ReactNode;
+  color: "green" | "yellow" | "red";
+}) {
+  return (
+    <li className="flex flex-col items-center gap-4 text-center">
+      <Icon id={icon} className="text-[60px] text-gray-700" />
+
+      <div className="w-full flex flex-col">
+        <h3
+          className={cn("font-serif text-[32px] font-bold leading-normal", {
+            "text-green-base": color === "green",
+            "text-yellow-darker": color === "yellow",
+            "text-red-base": color === "red",
+          })}
+        >
+          {value}
+        </h3>
+
+        <p>{label}</p>
+      </div>
     </li>
   );
 }
