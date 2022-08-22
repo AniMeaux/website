1802414185201ce5a8d354b7d95f1ca04af84321
Simon Relet <simon.relet@gmail.com>
2022-08-22 21:55:00 +0200

    🎪♻️ Use config in healthcheck route

--- apps/show/src/routes/healthcheck.tsx
+++ apps/show/src/routes/healthcheck.tsx
@@ -1,15 +1,15 @@
 // learn more: https://fly.io/docs/reference/configuration/#services-http_checks
 import { LoaderFunction } from "@remix-run/node";
+import { createConfig } from "~/core/config.server";
 
-export const loader: LoaderFunction = async ({ request }) => {
-  const host =
-    request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
+export const loader: LoaderFunction = async () => {
+  const config = createConfig();
 
   try {
     // We're good if we can:
     await Promise.all([
       // 1. Make a HEAD request to ourselves
-      fetch(`http://${host}`, { method: "HEAD" }).then((response) => {
+      fetch(config.publicHost, { method: "HEAD" }).then((response) => {
         if (!response.ok) {
           return Promise.reject(response);
         }
