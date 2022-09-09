6ab5d161e7a8eb033783bb1dccdd6529f8d4bc27
Simon Relet <simon.relet@gmail.com>
2022-09-09 18:55:39 +0200

    💿✨ Add other articles section

--- apps/website-remix/src/routes/blog.$slug.tsx
+++ apps/website-remix/src/routes/blog.$slug.tsx
@@ -2,6 +2,7 @@ import { json, LoaderFunction, MetaFunction } from "@remix-run/node";
 import { useCatch, useLoaderData } from "@remix-run/react";
 import { DateTime } from "luxon";
 import { Article, articles } from "~/blog/data";
+import { ArticleItem } from "~/blog/item";
 import { cn } from "~/core/classNames";
 import { getConfig } from "~/core/config";
 import { MapDateToString } from "~/core/dates";
@@ -10,10 +11,14 @@ import { getPageTitle } from "~/core/pageTitle";
 import { ErrorPage, getErrorTitle } from "~/dataDisplay/errorPage";
 import { createCloudinaryUrl, DynamicImage } from "~/dataDisplay/image";
 import { ARTICLE_COMPONENTS, Markdown } from "~/dataDisplay/markdown";
+import { LineShapeHorizontal } from "~/layout/lineShape";
 
 type LoaderDataServer = {
   article: Article;
+  otherArticles: Article[];
 };
+
+const OTHER_ARTICLE_COUNT = 3;
 
 export const loader: LoaderFunction = async ({ params }) => {
   const article = articles.find((article) => article.slug === params["slug"]);
@@ -21,7 +26,22 @@ export const loader: LoaderFunction = async ({ params }) => {
     throw new Response("Not found", { status: 404 });
   }
 
-  return json<LoaderDataServer>({ article });
+  let otherArticles = articles.filter(
+    (article) => article.slug !== params["slug"]
+  );
+
+  if (otherArticles.length > OTHER_ARTICLE_COUNT) {
+    const startIndex = Math.floor(
+      Math.random() * (otherArticles.length - OTHER_ARTICLE_COUNT + 1)
+    );
+
+    otherArticles = otherArticles.slice(
+      startIndex,
+      startIndex + OTHER_ARTICLE_COUNT
+    );
+  }
+
+  return json<LoaderDataServer>({ article, otherArticles });
 };
 
 export const meta: MetaFunction = ({ data, parentsData }) => {
@@ -50,35 +70,75 @@ export function CatchBoundary() {
 type LoaderDataClient = MapDateToString<LoaderDataServer>;
 
 export default function BlogPage() {
-  const { article } = useLoaderData<LoaderDataClient>();
+  const { article, otherArticles } = useLoaderData<LoaderDataClient>();
 
   return (
-    <main className="w-full px-article flex flex-col gap-12">
-      <header className={cn("px-4 flex flex-col gap-6", "md:px-0")}>
-        <h1 className={cn("text-title-hero-small", "md:text-title-hero-large")}>
-          {article.title}
-        </h1>
+    <>
+      <main className="w-full px-article flex flex-col gap-12">
+        <header className={cn("px-4 flex flex-col gap-6", "md:px-0")}>
+          <h1
+            className={cn("text-title-hero-small", "md:text-title-hero-large")}
+          >
+            {article.title}
+          </h1>
 
-        <p className="text-gray-500">
-          {DateTime.fromISO(article.publicationDate).toLocaleString(
-            DateTime.DATE_MED
-          )}{" "}
-          par {article.authorName}
-        </p>
-      </header>
+          <p className="text-gray-500">
+            {DateTime.fromISO(article.publicationDate).toLocaleString(
+              DateTime.DATE_MED
+            )}{" "}
+            par {article.authorName}
+          </p>
+        </header>
 
-      <DynamicImage
-        shouldFill
-        imageId={article.image}
-        alt={article.title}
-        sizes={{ lg: "1024px", default: "100vw" }}
-        fallbackSize="1024"
-        className="w-full aspect-4/3 flex-none rounded-bubble-ratio"
-      />
+        <DynamicImage
+          shouldFill
+          imageId={article.image}
+          alt={article.title}
+          sizes={{ lg: "1024px", default: "100vw" }}
+          fallbackSize="1024"
+          className="w-full aspect-4/3 flex-none rounded-bubble-ratio"
+        />
 
-      <article>
-        <Markdown components={ARTICLE_COMPONENTS}>{article.content}</Markdown>
-      </article>
-    </main>
+        <article>
+          <Markdown components={ARTICLE_COMPONENTS}>{article.content}</Markdown>
+        </article>
+      </main>
+
+      <aside
+        className={cn(
+          "w-full pt-[72px] px-page pb-12 flex flex-col items-center gap-12",
+          "md:py-12"
+        )}
+      >
+        <div className={cn("w-full px-2 flex", "md:px-6")}>
+          <LineShapeHorizontal
+            className={cn("w-full h-4 text-gray-300", "md:h-6")}
+          />
+        </div>
+
+        <div className="w-full flex flex-col gap-6">
+          <h2
+            className={cn(
+              "text-title-section-small",
+              "md:text-title-section-large"
+            )}
+          >
+            Continuer à lire
+          </h2>
+
+          <ul
+            className={cn(
+              "grid grid-cols-1 grid-rows-[auto] gap-6 items-start",
+              "xs:grid-cols-2",
+              "md:grid-cols-3"
+            )}
+          >
+            {otherArticles.map((article) => (
+              <ArticleItem key={article.id} article={article} />
+            ))}
+          </ul>
+        </div>
+      </aside>
+    </>
   );
 }
--- apps/website-remix/src/routes/blog.tsx
+++ apps/website-remix/src/routes/blog.tsx
@@ -1,13 +1,11 @@
 import { json, LoaderFunction, MetaFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
-import { DateTime } from "luxon";
 import { Article, articles } from "~/blog/data";
-import { BaseLink } from "~/core/baseLink";
+import { ArticleItem } from "~/blog/item";
 import { cn } from "~/core/classNames";
 import { MapDateToString } from "~/core/dates";
 import { createSocialMeta } from "~/core/meta";
 import { getPageTitle } from "~/core/pageTitle";
-import { DynamicImage } from "~/dataDisplay/image";
 
 type LoaderDataServer = {
   articles: Article[];
@@ -59,44 +57,5 @@ export default function BlogPage() {
         </p>
       )}
     </main>
-  );
-}
-
-function ArticleItem({
-  article,
-}: {
-  article: LoaderDataClient["articles"][number];
-}) {
-  return (
-    <li className="flex">
-      <BaseLink
-        to={`./${article.slug}`}
-        className={cn(
-          "w-full px-4 py-3 shadow-none rounded-bubble-lg bg-transparent flex flex-col gap-3 transition-[background-color,transform] duration-100 ease-in-out hover:bg-white hover:shadow-base",
-          "md:p-6"
-        )}
-      >
-        <DynamicImage
-          shouldFill
-          imageId={article.image}
-          alt={article.title}
-          sizes={{ lg: "300px", md: "30vw", xs: "50vw", default: "100vw" }}
-          fallbackSize="512"
-          className="w-full aspect-4/3 flex-none rounded-bubble-ratio"
-        />
-
-        <div className="flex flex-col">
-          <p className="text-caption-default text-gray-500">
-            {DateTime.fromISO(article.publicationDate).toLocaleString(
-              DateTime.DATE_MED
-            )}{" "}
-            par {article.authorName}
-          </p>
-
-          <p className="text-title-item">{article.title}</p>
-          <p>{article.description}</p>
-        </div>
-      </BaseLink>
-    </li>
   );
 }
