d21084385a65f48145f0746f944802e2d01e29ff
Simon Relet <simon.relet@gmail.com>
2022-09-22 22:18:11 +0200

    💿⏭ Replace Next by Remix

--- apps/website/src/pages/adopt/index.tsx
+++
@@ -1,9 +0,0 @@
-import { AdoptSearchParams } from "~/core/adoptSearchParams";
-import { createAdoptPage } from "~/elements/adopt/adoptPages";
-
-const { getServerSideProps, AdoptPage } = createAdoptPage(
-  new AdoptSearchParams()
-);
-
-export { getServerSideProps };
-export default AdoptPage;
