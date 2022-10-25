7ec129afc61c7114c73aa5c9e37b2d82a80e1952
Simon Relet <simon.relet@gmail.com>
2022-10-25 16:51:30 +0200

    🏢♻️ Move animal utils

---
+++ apps/faune-remix/src/animals/pickUp.ts
@@ -0,0 +1,9 @@
+import { PickUpReason } from "@prisma/client";
+
+export const PICK_UP_REASON_TRANSLATION: Record<PickUpReason, string> = {
+  [PickUpReason.ABANDONMENT]: "Abandon",
+  [PickUpReason.DECEASED_MASTER]: "Décès du propriétaire",
+  [PickUpReason.MISTREATMENT]: "Maltraitance",
+  [PickUpReason.STRAY_ANIMAL]: "Errance",
+  [PickUpReason.OTHER]: "Autre raison",
+};
