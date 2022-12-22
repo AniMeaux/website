7e2e6f2f709d06231c5f85206ffacb96dc298d6a
Simon Relet <simon.relet@gmail.com>
2022-12-22 23:23:14 +0100

    🏢✨ Allow desktop app minimal ui

--- apps/admin/src/routes/[manifest.json].tsx
+++ apps/admin/src/routes/[manifest.json].tsx
@@ -1,7 +1,8 @@
 import { getPageTitle } from "#/core/pageTitle";
 import { theme } from "#/generated/theme";
 import googleTouchIcon from "#/images/googleTouchIcon.png";
-import { json, LinksFunction } from "@remix-run/node";
+import { json, LinksFunction, LoaderArgs } from "@remix-run/node";
+import Bowser from "bowser";
 
 // Make sure `googleTouchIcon.png` is added to the build assets.
 // It looks like an asset only used on the server won't be included in the
@@ -10,13 +11,16 @@ export const links: LinksFunction = () => {
   return [{ rel: "preconnect", href: googleTouchIcon }];
 };
 
-export async function loader() {
+export async function loader({ request }: LoaderArgs) {
+  const browser = Bowser.getParser(request.headers.get("user-agent") ?? "");
+  const isDesktop = browser.getPlatformType() === "desktop";
+
   return json({
     name: getPageTitle(),
     short_name: getPageTitle(),
     background_color: theme.colors.white,
     theme_color: theme.colors.blue[500],
-    display: "standalone",
+    display: isDesktop ? "minimal-ui" : "standalone",
     scope: "/",
     start_url: "/?source=pwa",
     icons: [
