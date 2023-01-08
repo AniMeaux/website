b19d506c20e0efa6f47971304c23c38278af3e79
Simon Relet <simon.relet@gmail.com>
2023-01-08 19:43:22 +0100

    🏢✨ Update page titles

--- apps/admin/src/routes/__layout/animals.$id.edit.situation.tsx
+++ apps/admin/src/routes/__layout/animals.$id.edit.situation.tsx
@@ -76,7 +76,12 @@ export const meta: MetaFunction<typeof loader> = ({ data }) => {
     return { title: getPageTitle(getErrorTitle(404)) };
   }
 
-  return { title: getPageTitle(getAnimalDisplayName(animal)) };
+  return {
+    title: getPageTitle([
+      `Modifier ${getAnimalDisplayName(animal)}`,
+      "Situation",
+    ]),
+  };
 };
 
 type ActionData = {
