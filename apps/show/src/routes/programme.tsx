6d0443ca1169ce47c96a66088363650ffa46c61c
Simon Relet <simon.relet@gmail.com>
2023-01-15 23:39:55 +0100

    🎪✨ Add program

--- apps/show/src/routes/programme.tsx
+++ apps/show/src/routes/programme.tsx
@@ -1,29 +1,5 @@
-import { MetaFunction } from "@remix-run/node";
-import { cn } from "~/core/classNames";
-import { createSocialMeta } from "~/core/meta";
-import { getPageTitle } from "~/core/pageTitle";
+import { redirect } from "@remix-run/node";
 
-export const meta: MetaFunction = () => {
-  return createSocialMeta({ title: getPageTitle("Programme") });
-};
-
-export default function ProgramPage() {
-  return (
-    <main className="w-full px-page flex flex-col gap-12">
-      <header className="flex flex-col">
-        <h1
-          className={cn(
-            "w-full text-title-hero-small text-center",
-            "md:text-title-hero-large"
-          )}
-        >
-          Programme
-        </h1>
-      </header>
-
-      <p className={cn("py-12 text-center text-gray-500", "md:px-30 md:py-40")}>
-        Le programme sera transmis ultérieurement.
-      </p>
-    </main>
-  );
+export async function loader() {
+  throw redirect("/programme/samedi");
 }
