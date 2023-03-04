41354614e9ffa90895be22325592b5ddad462799
Simon Relet <simon.relet@gmail.com>
2023-03-04 22:33:59 +0100

    Revert "🌐⚙️ Use streams for rendering (React 18)"
    This reverts commit 0b3a305abf11e8da6c3a8535ff864e7c93faf5e6.
    
    react-transition-group doesn't support React 18 yet

--- apps/website/src/entry.server.tsx
+++ apps/website/src/entry.server.tsx
@@ -1,11 +1,7 @@
-import { EntryContext, Response } from "@remix-run/node";
+import { EntryContext } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
-import isbot from "isbot";
-import { renderToPipeableStream } from "react-dom/server";
-import { PassThrough } from "stream";
+import { renderToString } from "react-dom/server";
 import invariant from "tiny-invariant";
-
-const ABORT_DELAY = 5000;
 
 export default function handleRequest(
   request: Request,
@@ -13,52 +9,24 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  const callbackName = isbot(request.headers.get("user-agent"))
-    ? "onAllReady"
-    : "onShellReady";
+  const markup = renderToString(
+    <RemixServer context={remixContext} url={request.url} />
+  );
 
-  return new Promise((resolve, reject) => {
-    let didError = false;
+  responseHeaders.set("Content-Type", "text/html");
 
-    const { pipe, abort } = renderToPipeableStream(
-      <RemixServer context={remixContext} url={request.url} />,
-      {
-        [callbackName]: () => {
-          const body = new PassThrough();
+  if (process.env.NODE_ENV === "production") {
+    invariant(process.env.RUNTIME_ENV, "RUNTIME_ENV should be defined");
 
-          responseHeaders.set("Content-Type", "text/html");
+    if (process.env.RUNTIME_ENV === "staging") {
+      // We don't want it to be index by search engines.
+      // See https://developers.google.com/search/docs/advanced/crawling/block-indexing
+      responseHeaders.set("X-Robots-Tag", "noindex");
+    }
+  }
 
-          if (process.env.NODE_ENV === "production") {
-            invariant(process.env.RUNTIME_ENV, "RUNTIME_ENV should be defined");
-
-            if (process.env.RUNTIME_ENV === "staging") {
-              // We don't want it to be index by search engines.
-              // See https://developers.google.com/search/docs/advanced/crawling/block-indexing
-              responseHeaders.set("X-Robots-Tag", "noindex");
-            }
-          }
-
-          resolve(
-            new Response(body, {
-              headers: responseHeaders,
-              status: didError ? 500 : responseStatusCode,
-            })
-          );
-
-          pipe(body);
-        },
-
-        onShellError: (error: unknown) => {
-          reject(error);
-        },
-
-        onError: (error: unknown) => {
-          didError = true;
-          console.error("renderToPipeableStream:", error);
-        },
-      }
-    );
-
-    setTimeout(abort, ABORT_DELAY);
+  return new Response("<!DOCTYPE html>" + markup, {
+    status: responseStatusCode,
+    headers: responseHeaders,
   });
 }
