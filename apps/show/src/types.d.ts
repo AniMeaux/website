cb20a2b6d1edacb9eca301d1b911ff6753e75fbd
Simon Relet <simon.relet@gmail.com>
2023-03-05 18:50:49 +0100

    🏢🎪🌐🐛 Fix process.env typings

--- apps/show/src/types.d.ts
+++ apps/show/src/types.d.ts
@@ -1,13 +1,15 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    ANIMEAUX_URL?: string;
-    CLOUDINARY_CLOUD_NAME?: string;
-    FACEBOOK_URL?: string;
-    GOOGLE_TAG_MANAGER_ID?: string;
-    INSTAGRAM_URL?: string;
-    PRESS_RELEASE_URL?: string;
-    PUBLIC_HOST?: string;
-    RUNTIME_ENV?: "staging" | "production";
-    TICKETING_URL?: string;
+declare global {
+  declare namespace NodeJS {
+    interface ProcessEnv {
+      ANIMEAUX_URL?: string;
+      CLOUDINARY_CLOUD_NAME?: string;
+      FACEBOOK_URL?: string;
+      GOOGLE_TAG_MANAGER_ID?: string;
+      INSTAGRAM_URL?: string;
+      PRESS_RELEASE_URL?: string;
+      PUBLIC_HOST?: string;
+      RUNTIME_ENV?: "staging" | "production";
+      TICKETING_URL?: string;
+    }
   }
 }
