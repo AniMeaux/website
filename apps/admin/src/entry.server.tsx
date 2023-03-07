f716373eec7dd3ed8a5cd449a74b8ac20ea110de
Simon Relet <simon.relet@gmail.com>
2023-03-07 10:55:06 +0100

    🏢✨ Automatically extends user session

--- apps/admin/src/entry.server.tsx
+++ apps/admin/src/entry.server.tsx
@@ -1,18 +1,21 @@
-import { EntryContext } from "@remix-run/node";
+import { EntryContext, HandleDataRequestFunction } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import { renderToString } from "react-dom/server";
+import { extendSession } from "~/core/session.server";
 
 if (process.env.NODE_ENV === "development") {
   const { startWorker } = require("~/mocks/mocks.server");
   startWorker();
 }
 
-export default function handleRequest(
+export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
+  await extendSession(request.headers, responseHeaders);
+
   const markup = renderToString(
     <RemixServer context={remixContext} url={request.url} />
   );
@@ -28,3 +31,11 @@ export default function handleRequest(
     headers: responseHeaders,
   });
 }
+
+export const handleDataRequest: HandleDataRequestFunction = async (
+  response,
+  { request }
+) => {
+  await extendSession(request.headers, response.headers);
+  return response;
+};
