80be568d5fed530d79f9bb56044a33f5147f8006
Simon Relet <simon.relet@gmail.com>
2023-07-07 18:43:33 +0200

    🏢🎪🌐⬆️ Upgrade remix version

--- apps/website/remix.config.js
+++ apps/website/remix.config.js
@@ -7,7 +7,9 @@ module.exports = {
   serverModuleFormat: "cjs",
   tailwind: true,
   future: {
+    v2_dev: true,
     v2_errorBoundary: true,
+    v2_headers: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
