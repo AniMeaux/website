713de002746ec35aca88d3cd883ff809e36d0a52
Simon Relet <simon.relet@gmail.com>
2025-09-21 17:35:24 +0200

    ♻️🏢 Update with categories

---
+++ apps/admin/src/show/exhibitors/category.ts
@@ -0,0 +1,49 @@
+import { ActivityField } from "#show/exhibitors/activity-field/activity-field";
+import { LegalStatus } from "#show/exhibitors/applications/legal-status";
+import { ShowExhibitorCategory } from "@prisma/client";
+
+export namespace ExhibitorCategory {
+  export type Enum = ShowExhibitorCategory;
+  export const Enum = ShowExhibitorCategory;
+
+  export const translations: Record<Enum, string> = {
+    [Enum.ASSOCIATION]: "Association",
+    [Enum.SERVICE]: "Prestataire de service",
+    [Enum.SHOP]: "Boutique",
+  };
+
+  export function get({
+    legalStatus,
+    activityFields,
+  }: {
+    legalStatus: LegalStatus.Enum;
+    activityFields: ActivityField.Enum[];
+  }) {
+    if (
+      legalStatus === LegalStatus.Enum.ASSOCIATION ||
+      activityFields.includes(ActivityField.Enum.ASSOCIATION)
+    ) {
+      return Enum.ASSOCIATION;
+    }
+
+    const hasServiceActivityField = activityFields.some((activityField) =>
+      serviceActivityFields.includes(activityField),
+    );
+
+    if (hasServiceActivityField) {
+      return Enum.SERVICE;
+    }
+
+    return Enum.SHOP;
+  }
+
+  const serviceActivityFields = [
+    ActivityField.Enum.ALTERNATIVE_MEDICINE,
+    ActivityField.Enum.BEHAVIOR,
+    ActivityField.Enum.CITY,
+    ActivityField.Enum.EDUCATION,
+    ActivityField.Enum.SENSITIZATION,
+    ActivityField.Enum.SERVICES,
+    ActivityField.Enum.TRAINING,
+  ];
+}
