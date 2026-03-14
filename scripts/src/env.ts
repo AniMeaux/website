4a387262f891cfd4f9be1f4a773ae6a7dfbbe211
Simon Relet <simon.relet@gmail.com>
2026-03-14 10:19:03 +0100

    ⚙️ Add ESLint to scripts lib

--- scripts/src/env.ts
+++ scripts/src/env.ts
@@ -1,5 +1,6 @@
-import { config } from "dotenv-flow";
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
+
+import { config } from "dotenv-flow";
 
 config({ path: resolve(dirname(fileURLToPath(import.meta.url)), "../../") });
